652f3220e27f259951fd88496867e47ffd6cc731
seoshrestha <chandra.shrestha@digitaltransformationsystem.com>
2024-10-11 01:47:07 +0900

    Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //this scanner hold the data
+        Scanner myObj = new Scanner(System.in);
+        // send the message to the user for input data
+
+        System.out.println("Please inter a word");
+
+        String anyword = myObj.nextLine();
+        //Out
+
+        System.out.println("Hello =>"+anyword);
+
     }
 }
